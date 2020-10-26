8e3455a00b4d60917676135c043e5823416d6a67
aobertas <aobertas@gmail.com>
2020-10-26 14:53:38 -0400

    reading file1 and file2 separately and saving into list

--- fang-margot-2013-comparison/generate_restart_list.py
+++ fang-margot-2013-comparison/generate_restart_list.py
@@ -9,17 +9,24 @@ output = str(args[3])
 
 i=0
 
-with open(file1, "r") as f1:
-    with open(file2, "r") as f2:
-        with open(output, "a") as of:
-            while i < 20:
-                archive_name = f1.readline()
-                linenum = int(f2.readline().strip())
+with open(file1, "r") as f:
+    file1_data = f.read()
 
-                while i <= linenum:
+with open(file2, "r") as f:
+    file2_data = f.read()
 
-                    print(archive_name, linenum, i)
+print(file1_data[0:10])
 
-                    archive_name = f1.readline()
-                    # of.write(str(archive_name) + "\n")
-                    i += 1
+# with open(file2, "r") as f2:
+#         with open(output, "a") as of:
+#             while i < 20:
+#                 archive_name = f1.readline()
+#                 linenum = int(f2.readline().strip())
+
+#                 while i <= linenum:
+
+#                     print(archive_name, linenum, i)
+
+#                     archive_name = f1.readline()
+#                     # of.write(str(archive_name) + "\n")
+#                     i += 1
