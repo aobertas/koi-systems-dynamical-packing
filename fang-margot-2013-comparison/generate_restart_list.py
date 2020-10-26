db4320794381efbe78c6dbb9b1ef76747ffd8d97
aobertas <aobertas@gmail.com>
2020-10-26 16:25:38 -0400

    printing first 10 items in lists

--- fang-margot-2013-comparison/generate_restart_list.py
+++ fang-margot-2013-comparison/generate_restart_list.py
@@ -15,7 +15,7 @@ with open(file1, "r") as f:
 with open(file2, "r") as f:
     file2_data = f.readlines()
 
-print(file1_data[0:10])
+print(file1_data[0:10], file2_data[0:10])
 
 # with open(file2, "r") as f2:
 #         with open(output, "a") as of:
