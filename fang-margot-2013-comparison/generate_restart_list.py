f27ad645b581114e5519fbe773f8a006004f0439
aobertas <aobertas@gmail.com>
2020-10-26 14:21:02 -0400

    added for loop (oops)

--- fang-margot-2013-comparison/generate_restart_list.py
+++ fang-margot-2013-comparison/generate_restart_list.py
@@ -7,16 +7,14 @@ file1= str(args[1])
 file2 = str(args[2])
 output = str(args[3])
 
-i=0
-
 with open(file1, "r") as f1:
     with open(file2, "r") as f2:
         with open(output, "a") as of:
-            archive_name = f1.readline()
-            linenum = f2.readline()
+            for i in range(920):
+                archive_name = f1.readline()
+                linenum = int(f2.readline())
 
-            if linenum == i:
-                of.write(str(archive_name) + "\n")
-            else:
-                f1.readline()
-            i += 1
+                if linenum == i:
+                    of.write(str(archive_name) + "\n")
+                else:
+                    f1.readline()
