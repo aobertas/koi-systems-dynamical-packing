fa2cbe50266b1d4f3cba4f25b5d9024ac0abc875
aobertas <aobertas@gmail.com>
2020-10-26 11:45:46 -0400

    changed loop syntax for two files

--- fang-margot-2013-comparison/generate_restart_list.sh
+++ fang-margot-2013-comparison/generate_restart_list.sh
@@ -5,7 +5,7 @@ iter=0
 input1="initial_run_lines_sorted"
 input2="koi-gaia-initial.txt"
 
-while IFS= read -r f1 && read -r f2
+while IFS= read -r f1 <&3 && read -r f2 <&4
 do
     echo $f1 $f2 $iter
     # if [ $f1 == $iter ]
@@ -14,4 +14,4 @@ do
     # fi
 
     ((iter++))
-done < "$input1" < "$input2"
+done 3<$input1 4<$input2
