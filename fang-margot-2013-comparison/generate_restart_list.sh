ab49fb181613da77929438cd519b3eceed45db53
aobertas <aobertas@gmail.com>
2020-10-26 11:41:00 -0400

    print lines instead

--- fang-margot-2013-comparison/generate_restart_list.sh
+++ fang-margot-2013-comparison/generate_restart_list.sh
@@ -7,10 +7,11 @@ input2="koi-gaia-initial.txt"
 
 while IFS= read -r f1 && read -r f2
 do
-    if [ $f1 == $iter ]
-    then
-        echo $f2 $iter
-    fi
+    echo $f1 $f2 $iter
+    # if [ $f1 == $iter ]
+    # then
+        # echo $f2 $iter
+    # fi
 
     ((iter++))
 done < "$input1" < "$input2"
