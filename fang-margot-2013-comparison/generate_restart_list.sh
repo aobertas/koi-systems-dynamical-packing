b327ca9a58c52c3ce78d16305e611dd787a19183
aobertas <aobertas@gmail.com>
2020-10-26 13:16:40 -0400

    trying if/else statement

--- fang-margot-2013-comparison/generate_restart_list.sh
+++ fang-margot-2013-comparison/generate_restart_list.sh
@@ -7,11 +7,16 @@ input2="initial_run_lines_sorted"
 
 while IFS= read -r f1 <&3 && read -r f2 <&4
 do
-    echo $f1 $f2 $iter
+    if [ $f2 == $iter ]
+    then
+        echo $f1 $f2 $iter
+        ((iter++))
+    else
+        echo $f1 $f2 $iter
+        iter=$f2
+    fi
     # if [ $f1 == $iter ]
     # then
         # echo $f2 $iter
     # fi
-
-    ((iter++))
 done 3<$input1 4<$input2
