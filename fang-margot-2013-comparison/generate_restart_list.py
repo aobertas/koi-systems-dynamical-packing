f37ea5766b1f59fbb7cb449b7b2e3678d1d8297b
aobertas <aobertas@gmail.com>
2020-10-26 14:33:30 -0400

    changed from for loop to write; remove whitespace

--- fang-margot-2013-comparison/generate_restart_list.py
+++ fang-margot-2013-comparison/generate_restart_list.py
@@ -7,12 +7,14 @@ file1= str(args[1])
 file2 = str(args[2])
 output = str(args[3])
 
+i=0
+
 with open(file1, "r") as f1:
     with open(file2, "r") as f2:
         with open(output, "a") as of:
-            for i in range(920):
+            while i < 920:
                 archive_name = f1.readline()
-                linenum = f2.readline()
+                linenum = f2.readline().strip()
 
                 print(linenum)
 
@@ -20,3 +22,4 @@ with open(file1, "r") as f1:
                 #     of.write(str(archive_name) + "\n")
                 # else:
                 #     f1.readline()
+                i += 1
