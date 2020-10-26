0519eac1733477fe0f0eb550dc1990cf382a7954
aobertas <aobertas@gmail.com>
2020-10-26 11:03:38 -0400

    new script for generating restart list

---
+++ fang-margot-2013-comparison/generate_restart_list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+iter=0
+
+input1="initial_run_lines_sorted"
+input2="koi-gaia-initial.txt"
+
+while IFS= read -r f1 && read -r f2
+do
+    if [ $f1 == $iter ]
+        echo $f2 >> koi-gaia-first.txt
+    fi
+
+    ((iter++))
+done < $input1 $input2
