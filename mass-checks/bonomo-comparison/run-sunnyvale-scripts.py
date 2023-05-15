0b88bdaa8dca8bceffa1f0fb0595cf66aa350669
aobertas <aobertas@gmail.com>
2023-05-15 14:59:20 -0400

    now having it print the os.system text

--- mass-checks/bonomo-comparison/run-sunnyvale-scripts.py
+++ mass-checks/bonomo-comparison/run-sunnyvale-scripts.py
@@ -11,12 +11,13 @@ def run_sunnyvalue_scripts(df, dirname, n_samp):
         N_planets = len(df[df['kepid'] == sysid])
         N_pair = N_planets - 1
 
-        print(sysid)
-
-        # for i in range(N_pair):
-        #     os.system("qsub -v sysid=" + str(sysid) + ",place=" + str(i) + \
-        #         ",dirname=" + dirname + ",n_samp=" + str(n_samp) + \
-        #         " sunnyvale-qsub-script-spock-probs")
+        for i in range(N_pair):
+            print("qsub -v sysid=" + str(sysid) + ",place=" + str(i) + \
+                ",dirname=" + dirname + ",n_samp=" + str(n_samp) + \
+                " sunnyvale-qsub-script-spock-probs")
+            # os.system("qsub -v sysid=" + str(sysid) + ",place=" + str(i) + \
+            #     ",dirname=" + dirname + ",n_samp=" + str(n_samp) + \
+            #     " sunnyvale-qsub-script-spock-probs")
 
 #######################################################################
 
