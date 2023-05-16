8b0fb00a8c89860091d4c30a923c762134fd9b96
aobertas <aobertas@gmail.com>
2023-05-15 20:26:55 -0400

    reverted back to submitting jobs using os.system

--- mass-checks/bonomo-comparison/run-sunnyvale-scripts.py
+++ mass-checks/bonomo-comparison/run-sunnyvale-scripts.py
@@ -14,12 +14,9 @@ def run_sunnyvalue_scripts(df, dirname, n_samp):
             N_pair = N_planets - 1
 
             for i in range(N_pair):
-                print("qsub -v sysid=" + str(sysid) + ",place=" + str(i) + \
+                os.system("qsub -v sysid=" + str(sysid) + ",place=" + str(i) + \
                     ",dirname=" + dirname + ",n_samp=" + str(n_samp) + \
                     " sunnyvale-qsub-script-spock-probs")
-                # os.system("qsub -v sysid=" + str(sysid) + ",place=" + str(i) + \
-                #     ",dirname=" + dirname + ",n_samp=" + str(n_samp) + \
-                #     " sunnyvale-qsub-script-spock-probs")
 
 #######################################################################
 
